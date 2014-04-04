5962afc38fdd27c137d0133e72c3aa0ae7636213
mfsegura <mfsegura@gmail.com>
2014-04-04 09:14:36 +0200

    Actualizacion paquete Service

---
+++ si3/src/main/java/com/practica/si3/services/OfertaService.java
@@ -0,0 +1,12 @@
+package com.practica.si3.services;
+
+import java.util.List;
+import com.practica.si3.domain.Oferta;
+
+
+
+public interface OfertaService {
+
+	public void insertData(Oferta oferta);
+	public List<Oferta> getOfertaList();
+}
